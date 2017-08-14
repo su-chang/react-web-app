65c6d9126afc57f994fd4522898364016263aca3
Su Chang <su_chang@yeah.net>
2017-08-14 22:29:06 +0800

    feat: 商家信息展示组件 1. 获取商家信息 2. 定义Star组件 3. 防XSS攻击

---
+++ app/containers/Detail/subpage/info.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { getInfoData } from '../../../fetch/detail/detail'
+import DetailInfo from '../../../components/DetailInfo'
+
+class Info extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+            info: false
+        }
+    }
+    render() {
+        return (
+            <div>
+                {
+                    this.state.info
+                        ? <DetailInfo data={this.state.info}/>
+                        : ''
+                }
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        const id = this.props.id;
+        let result = getInfoData(id);
+        result.then(res => {
+            return res.json();
+        }).then(json => {
+            this.setState({
+                info: json
+            })
+        })
+    }
+}
+
+// 使用 require.ensure 异步加载，还不支持 ES6 的 export
+// export default NotFound
+export default Info

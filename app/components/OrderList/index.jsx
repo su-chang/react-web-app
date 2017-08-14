dfd1428fd0f6fee182a624a6b0852c2338490a32
Su Chang <su_chang@yeah.net>
2017-08-15 05:36:48 +0800

    feat: 评价功能

--- app/components/OrderList/index.jsx
+++ app/components/OrderList/index.jsx
@@ -11,10 +11,11 @@ class List extends React.Component {
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
     render() {
+        const submitComment = this.props.submitComment;
         return (
             <div className="list-container">
                 {this.props.data.map((item, index) => {
-                    return <Item key={index} data={item}/>
+                    return <Item key={index} data={item} submitComment={submitComment}/>
                 })}
             </div>
         )
--- app/containers/User/subpage/OrderList.jsx
+++ app/containers/User/subpage/OrderList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import OrderListComponent from '../../../components/OrderList'
-import { getOrderListData } from '../../../fetch/user/orderlist'
+import { getOrderListData,postComment } from '../../../fetch/user/orderlist'
 
 class OrderList extends React.Component {
     constructor(props, context) {
@@ -17,7 +17,7 @@ class OrderList extends React.Component {
                 <h2>您的订单</h2>
                 {
                     this.state.data.length
-                        ? <OrderListComponent data={this.state.data}/>
+                        ? <OrderListComponent data={this.state.data} submitComment={this.submitComment.bind(this)}/>
                         : <div>{/* loading */}</div>
                 }
             </div>
@@ -43,6 +43,19 @@ class OrderList extends React.Component {
         }).catch(ex => {
             if (__DEV__) {
                 console.error('首页”猜你喜欢“获取数据报错, ', ex.message)
+            }
+        })
+    }
+
+    // 提交评价
+    submitComment(id , value, callback) {
+        const result = postComment(id, value);
+        result.then(res => {
+            return res.json();
+        }).then(json => {
+            if (json.errno === 0) {
+                // 已经评价，修改状态
+                callback();
             }
         })
     }
